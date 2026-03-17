07d3a2d8b111c82c0f8f73988ae40175f226e733
MarquessVinolust <mpumi@one-four-six.com>
2026-03-17 15:14:18 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
         <motion.img
           src={logo}
           alt="Brice & Burnett"
-          className="h-10"
+          className="h-7"
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
